4c748281e47f9d1837bafa5b1f0cf3b71acc1893
Tarushi Sandeep Gupta <21BCS6280@cuchd.in>
2023-07-30 04:25:04 +0530

    token

---
+++ minttoken.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _initialSupply * 10**uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function mint(address _to, uint256 _amount) public {
+        // Add necessary minting conditions here (e.g., only the owner can mint)
+        balanceOf[_to] += _amount;
+        totalSupply += _amount;
+        emit Transfer(address(0), _to, _amount);
+    }
+
+    function burn(uint256 _amount) public {
+        require(balanceOf[msg.sender] >= _amount, "Insufficient balance to burn");
+        balanceOf[msg.sender] -= _amount;
+        totalSupply -= _amount;
+        emit Transfer(msg.sender, address(0), _amount);
+    }
+}
